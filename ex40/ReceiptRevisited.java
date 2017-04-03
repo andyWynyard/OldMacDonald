478fa63a8627db4ade29f373a884e782e0929c95
andyWynyard <andrew.wynyard@gmail.com>
2017-04-03 18:06:55 +0200

    first commit

---
+++ ex40/ReceiptRevisited.java
@@ -0,0 +1,9 @@
+import java.io.PrintWriter;
+
+public class ReceiptRevisited {
+	public static void main(String[]args) throws Exception {
+		PrintWriter fileout = new PrintWriter("receipt.txt");
+
+		fileout.println( "+---------------------+" );
+	}
+}
